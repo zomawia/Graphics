fd728f3f8f8b6bfbfad0bf372d6d06cad904a9ae
s179140 <s179140@SEA-7FF209>
2017-08-30 17:55:55 -0700

    phuong

--- resources/shaders/standard.frag
+++ resources/shaders/standard.frag
@@ -7,20 +7,38 @@ in mat4 vTBN;
 
 out vec4 outColor;
 
+layout (location = 1) uniform mat4 view;
 layout (location = 3) uniform sampler2D diffuse;
-layout (location = 4) uniform vec3 lightPos;
-layout (location = 5) uniform vec4 lightColor;
+layout (location = 4) uniform sampler2D normal;
+layout (location = 5) uniform vec3 lightPos;
+layout (location = 6) uniform vec4 lightColor;
+
+float lambert(vec3 N, vec3 L)
+{
+	return max(0, dot(N, -L));
+}
+
+float phong(vec3 N, vec3 L, vec3 V, float power)
+{
+	vec3 R = reflect(L, N);	
+	float base = max(0, dot(V, R));
+	return pow(base, power);
+}
 
 void main()
 {
-    vec4 surfaceColor = texture(diffuse, vUV);
     
-	vec3 vNormal = vPos.xyz;
+    vec4 tN = 1-2*texture(normal, vUV);
+	tN.a = 0;
+	tN = normalize(tN);
 
-    float lambertianFactor = max(dot(vNormal, -lightPos), 0);
+	vec3 N = (vTBN * tN).xyz;
+	vec3 V = view[2].xyz;
 
-    vec4 lambertianTerm = surfaceColor * lambertianFactor * lightColor;
+	float spec = phong(N, lightPos ,V, 0.8);
+	float lam = lambert(N, lightPos);
+	
 
-
-    outColor = lambertianTerm;
+    outColor += texture(diffuse, vUV) * lam +
+				(vec4(1,1,1,1) * spec);
 }
--- tests/07-Materials/main.cpp
+++ tests/07-Materials/main.cpp
@@ -34,33 +34,66 @@ int main()
 
 	Framebuffer screen = { 0, 1280, 800 };
 	glm::mat4 proj = glm::perspective(45.f, 1280.f / 800.f, 0.01f, 100.f);
-	glm::mat4 view = glm::lookAt(glm::vec3(0, 3, -8),
+	glm::mat4 view = glm::lookAt(glm::vec3(0, 2, -2),
 		glm::vec3(0, 1, 0), glm::vec3(0, 1, 0));
 
 	glm::vec4 lightCol = glm::vec4(1, 1, 1, 1);
-	glm::vec3 lightDir = glm::normalize(glm::vec3(1, -1, 1));
+	glm::vec3 lightDir = glm::normalize(glm::vec3(3, -5, 1));
 
 	while (context.step())
 	{
 		float time = context.getTime();
 		glm::mat4 model = glm::rotate(time, glm::vec3(0, 1, 0));
-		glm::mat4 modquad = glm::rotate(
-			glm::radians(90.f), 
+		glm::mat4 modquad = glm::rotate(time, glm::vec3(1, -1, 0));
+		
+		glm::mat4 modquad0 = glm::rotate(
+			glm::radians(90.f),
 			glm::vec3(-1, 0, 0)) * glm::scale(glm::vec3(5, 5, 1));
+		glm::mat4 modquad1 = glm::rotate(time * 2, glm::vec3(0, 1, 0));
+		glm::mat4 modquad2 = glm::rotate(time * 5, glm::vec3(0, -1, 1));
+		glm::mat4 modquad3 = modquad * glm::translate(glm::vec3(5, -5, 0));
+		glm::mat4 modquad4 = modquad * glm::translate(glm::vec3(5, 0, 0));
+		glm::mat4 modquad5 = modquad * glm::translate(glm::vec3(-5, 0, 0));
+		glm::mat4 modquad6 = modquad * glm::translate(glm::vec3(-5, 5, 0));
 
 		setFlags(RenderFlag::DEPTH);
 		clearFramebuffer(screen);
 
 		int loc = 0, slot = 0;
-		setUniforms(standard, loc, slot, proj, view, model, ss_diffuse, lightDir, lightCol);
+		setUniforms(standard, loc, slot, proj, view, model, ss_diffuse, ss_normal, lightDir, lightCol);
 		s0_draw(screen, standard, ss_geo);
 
 		loc = 0, slot = 0;
-		setUniforms(standard, loc, slot, proj, view, modquad, floor_diffuse, lightDir, lightCol);
+		setUniforms(standard, loc, slot, proj, view, modquad, floor_diffuse, floor_normal, lightDir, lightCol);
 		s0_draw(screen, standard, quad);
 
 		loc = 0, slot = 0;
-		setUniforms(standard, loc, slot, proj, view, modquad, floor_normal, lightDir, lightCol);
+		setUniforms(standard, loc, slot, proj, view, modquad0, floor_diffuse, floor_normal, lightDir, lightCol);
+		s0_draw(screen, standard, quad);
+
+		loc = 0, slot = 0;
+		setUniforms(standard, loc, slot, proj, view, modquad1, floor_diffuse, floor_normal, lightDir, lightCol);
+		s0_draw(screen, standard, quad);
+
+		loc = 0, slot = 0;
+		setUniforms(standard, loc, slot, proj, view, modquad2, floor_diffuse, floor_normal, lightDir, lightCol);
+		s0_draw(screen, standard, quad);
+
+		loc = 0, slot = 0;
+		setUniforms(standard, loc, slot, proj, view, modquad3, floor_diffuse, floor_normal, lightDir, lightCol);
+		s0_draw(screen, standard, quad);
+		loc = 0, slot = 0;
+		setUniforms(standard, loc, slot, proj, view, modquad4, floor_diffuse, floor_normal, lightDir, lightCol);
+		s0_draw(screen, standard, quad);
+		loc = 0, slot = 0;
+		setUniforms(standard, loc, slot, proj, view, modquad5, floor_diffuse, floor_normal, lightDir, lightCol);
+		s0_draw(screen, standard, quad);
+		loc = 0, slot = 0;
+		setUniforms(standard, loc, slot, proj, view, modquad6, floor_diffuse, floor_normal, lightDir, lightCol);
+		s0_draw(screen, standard, quad);
+
+		loc = 0, slot = 0;
+		setUniforms(standard, loc, slot, proj, view, modquad, floor_normal, floor_normal, lightDir, lightCol);
 		s0_draw(screen, standard, quad);
 	}
 
